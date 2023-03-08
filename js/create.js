f873e5beb310fb8a9642468d0cf39b961de040b8
nikonew <spiritofadv@yandex.ru>
2023-03-08 13:27:12 +0300

    поправил отступы и кавычки

--- js/create.js
+++ js/create.js
@@ -1,27 +1,27 @@
-import {PICTURE_COUNT,AVATAR_COUNT,LIKE_MIN_COUNT,LIKE_MAX_COUNT,COMMENT_COUNT,COMMENT_MESSAGES,DESCRIPTIONS,NAMES} from "./data.js"
-import { getRandomArrayElement, getRandomInteger } from "./get-random-integer.js";
+import {PICTURE_COUNT,AVATAR_COUNT,LIKE_MIN_COUNT,LIKE_MAX_COUNT,COMMENT_COUNT,COMMENT_MESSAGES,DESCRIPTIONS,NAMES} from './data.js';
+import { getRandomArrayElement, getRandomInteger } from './get-random-integer.js';
 
 function createIdGenerator () {
-    let lastGeneratedId = 0;
-  
-    return function () {
-      lastGeneratedId += 1;
-      return lastGeneratedId;
-    };
+  let lastGeneratedId = 0;
+
+  return function () {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
   };
-   const generatorComment = createIdGenerator();
+}
+const generatorComment = createIdGenerator();
 
 // функция для создания сообщения
 const createMessage = () =>
-Array.from({ length: getRandomInteger(1, 2)}, () =>
-getRandomArrayElement(COMMENT_MESSAGES)).join(' ');
+  Array.from({ length: getRandomInteger(1, 2)}, () =>
+    getRandomArrayElement(COMMENT_MESSAGES)).join(' ');
 
 // функция для создания комментария
 const createComment = () =>({
-id: generatorComment(),
-url: `img/avatar-${getRandomInteger(1, AVATAR_COUNT).svg}`, // использование шаблонной строки ${}
-massage: createMessage(),
-name: getRandomArrayElement(NAMES),
+  id: generatorComment(),
+  url: `img/avatar-${getRandomInteger(1, AVATAR_COUNT).svg}`, // использование шаблонной строки ${}
+  massage: createMessage(),
+  name: getRandomArrayElement(NAMES),
 });
 
 // функция создания изображения
@@ -38,10 +38,10 @@ const createPicture = (index) => ({
 
 // функция выбирает изображение
 const getPictures = () =>
-Array.from({ length: PICTURE_COUNT }, (_, pictureIndex) =>
- createPicture(pictureIndex + 1)
-);
+  Array.from({ length: PICTURE_COUNT }, (_, pictureIndex) =>
+    createPicture(pictureIndex + 1)
+  );
 getPictures();
 console.log(getPictures());
 
-export { createMessage, createComment, createPicture, getPictures, createIdGenerator }
+export { createMessage, createComment, createPicture, getPictures, createIdGenerator};
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-import { getRandomArrayElement, getRandomInteger } from "./get-random-integer.js";
-import { PICTURE_COUNT,AVATAR_COUNT,LIKE_MIN_COUNT,LIKE_MAX_COUNT,COMMENT_COUNT,COMMENT_MESSAGES,DESCRIPTIONS,NAMES } from "./data.js"
-import {createMessage, createComment, createPicture, getPictures, createIdGenerator } from "./create.js"
+import { getRandomArrayElement, getRandomInteger } from './get-random-integer.js';
+import { PICTURE_COUNT,AVATAR_COUNT,LIKE_MIN_COUNT,LIKE_MAX_COUNT,COMMENT_COUNT,COMMENT_MESSAGES,DESCRIPTIONS,NAMES } from './data.js';
+import {createMessage, createComment, createPicture, getPictures, createIdGenerator } from './create.js';
 
 
