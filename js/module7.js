93f983675e3330fb06ade99371eba55c8a1c8c91
nikonew <spiritofadv@yandex.ru>
2023-03-22 23:16:21 +0300

    дополнил код

--- js/module7.js
+++ js/module7.js
@@ -1,13 +1,17 @@
+import { getPictures } from './create.js';
 
-const templatePicture = document.querySelector('#picture').content;
-const picturesContainer = document.querySelector('.picture');
+const templatePicture = document.querySelector('#picture').content.querySelector('.picture');
+const picturesContainer = document.querySelector('.pictures');
 const pictureFragment = document.createDocumentFragment();
 
-const createPictures = ({url, likes, comments}) => {
+const similarPictures = getPictures();
+
+similarPictures.forEach((url, likes, comments) => {
   const userPicture = templatePicture.cloneNode(true);
   userPicture.querySelector('.picture__img').src = url;
   userPicture.querySelector('.picture__like').textContent = likes;
   userPicture.querySelector('.picture__comments').textContent = comments;
-};
+  picturesContainer.append(userPicture);
+});
 
-createPictures(pictureFragment);
+picturesContainer.appendChild(pictureFragment);
