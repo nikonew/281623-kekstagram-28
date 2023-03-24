21723b40cd2d997afc1287c27e80623c748671fe
nikonew <spiritofadv@yandex.ru>
2023-03-24 08:17:14 +0300

    Подготовил задание

--- js/template.js
+++ js/template.js
@@ -1,17 +1,19 @@
 import { getPictures } from './create.js';
 
-const templatePicture = document.querySelector('#picture').content.querySelector('.picture');
+const templatePicture = document.querySelector('#picture').content;
 const picturesContainer = document.querySelector('.pictures');
 const pictureFragment = document.createDocumentFragment();
 
 const similarPictures = getPictures();
 
-similarPictures.forEach((url, likes, comments) => {
+similarPictures.forEach((imageData, likes, comments) => {
   const userPicture = templatePicture.cloneNode(true);
-  userPicture.querySelector('.picture__img').src = url;
-  userPicture.querySelector('.picture__like').textContent = likes;
+  userPicture.querySelector('.picture__img').src = imageData.url;
+  userPicture.querySelector('.picture__likes').textContent = likes;
   userPicture.querySelector('.picture__comments').textContent = comments;
   picturesContainer.append(userPicture);
 });
 
 picturesContainer.appendChild(pictureFragment);
+
+
